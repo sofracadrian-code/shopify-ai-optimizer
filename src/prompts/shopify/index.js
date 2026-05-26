14ce70794bbc2352ce67a86f5c14118331d626e0
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-27 00:25:59 +0200

    Update index.js

--- src/prompts/shopify/index.js
+++ src/prompts/shopify/index.js
@@ -1,1 +1,6 @@
-
+export { SHOPIFY_ELECTRONICS_PROMPT } from './electronics.js';
+export { SHOPIFY_MOBILITY_PROMPT } from './mobility.js';
+export { SHOPIFY_FASHION_PROMPT } from './fashion.js';
+export { SHOPIFY_HOME_PROMPT } from './home.js';
+export { SHOPIFY_TOOLS_PROMPT } from './tools.js';
+export { SHOPIFY_DEFAULT_PROMPT } from './default.js';
