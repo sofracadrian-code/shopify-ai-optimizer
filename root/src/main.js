3b0ca7d528bd54e87abc77118e10cd41d02e0da7
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-29 00:15:32 +0200

    Update main.js

--- root/src/main.js
+++ root/src/main.js
@@ -1,1 +1,17 @@
+import dotenv from 'dotenv';
+import { generateSEO } from './src/lib/generateSEO.js';
 
+dotenv.config();
+
+const product = {
+title: 'POCO F7 Pro',
+description: 'Smartphone with AMOLED display'
+};
+
+async function test() {
+const seo = await generateSEO(product);
+
+console.log(seo);
+}
+
+test();
