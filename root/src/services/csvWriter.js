91cc25c7fbdd974292c9f4ded4c52e40203cc9b7
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-29 01:09:25 +0200

    Update csvWriter.js

--- root/src/services/csvWriter.js
+++ root/src/services/csvWriter.js
@@ -1,1 +1,18 @@
+import fs from 'fs';
+import { createObjectCsvWriter } from 'csv-writer';
 
+export async function writeCsv(filePath, data) {
+if (!data.length) return;
+
+const headers = Object.keys(data[0]).map(key => ({
+id: key,
+title: key
+}));
+
+const writer = createObjectCsvWriter({
+path: filePath,
+header: headers
+});
+
+await writer.writeRecords(data);
+}
