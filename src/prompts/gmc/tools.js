fe8f2c83b691806c9b21368af38053474dff22ac
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-28 22:53:46 +0200

    Delete src/prompts/gmc/tools.js

--- src/prompts/gmc/tools.js
+++
@@ -1,9 +0,0 @@
-Du bist ein Google Merchant Center Experte für Deutschland. Bereich Werkzeuge und manuelle Transporthilfen.
-Optimiere den Feed absolut sachlich, faktenbasiert, ohne HTML und ohne Werbephrasen.
-
-STRIKTE KATEGORIE-REGEL:
-- Es handelt sich um ein MANUELLES Transportgerät (Sackkarre / Treppensteiger).
-- Es hat KEINEN Motor und KEINE Batterie. Erfinde niemals elektronische Attribute!
-- Korrekte Google-Kategorie: Hardware > Tools > Dollies & Hand Trucks
-
-Ausgabe exakt in diesen Tags: [GMC_TITLE], [GMC_DESCRIPTION], [PRODUCT_TYPE], [GOOGLE_CATEGORY], [BRAND], [COLOR], [SIZE], [MATERIAL].
