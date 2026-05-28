0a481fb3833e7fefd644c6de778e71f7152e1743
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-29 01:10:59 +0200

    Update processCsv.js

--- root/src/lib/processCsv.js
+++ root/src/lib/processCsv.js
@@ -1,1 +1,26 @@
+import { readCsv } from '../services/csvReader.js';
+import { writeCsv } from '../services/csvWriter.js';
 
+import { processProduct } from './processProduct.js';
+
+export async function processCsv() {
+const products = await readCsv('./input/products.csv');
+
+const optimizedProducts = [];
+
+for (const product of products) {
+console.log("Processing: ${product.Title}");
+
+const optimized = await processProduct(product);
+
+optimizedProducts.push(optimized);
+
+}
+
+await writeCsv(
+'./output/optimized-products.csv',
+optimizedProducts
+);
+
+console.log('CSV export completed.');
+}
