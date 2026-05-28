9fd7999e97c61d0b3169fd00b70c79e58f7cba71
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-28 22:53:21 +0200

    Delete src/prompts/gmc/index.js

--- src/prompts/gmc/index.js
+++
@@ -1,6 +0,0 @@
-export { GMC_ELECTRONICS_PROMPT } from './electronics.js';
-export { GMC_MOBILITY_PROMPT }    from './mobility.js';
-export { GMC_FASHION_PROMPT }     from './fashion.js';
-export { GMC_HOME_PROMPT }        from './home.js';
-export { GMC_TOOLS_PROMPT }       from './tools.js';
-export { GMC_DEFAULT_PROMPT }     from './default.js';
