75ffbb039d7221e3f35ff0857eabea2c6cb4c60b
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-29 01:07:47 +0200

    Update csvReader.js

--- root/src/services/csvReader.js
+++ root/src/services/csvReader.js
@@ -1,1 +1,15 @@
+import fs from 'fs';
+import csv from 'csv-parser';
 
+export async function readCsv(filePath) {
+return new Promise((resolve, reject) => {
+const results = [];
+
+fs.createReadStream(filePath)
+  .pipe(csv())
+  .on('data', data => results.push(data))
+  .on('end', () => resolve(results))
+  .on('error', reject);
+
+});
+}
