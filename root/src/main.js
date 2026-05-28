1b52f6ea73194b37f2ff08cdabf52df9678cd60c
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-29 00:23:44 +0200

    Update main.js

--- root/src/main.js
+++ root/src/main.js
@@ -4,11 +4,20 @@ import { generateText } from './src/services/openai.js';
 dotenv.config();
 
 async function test() {
+try {
+console.log('Testing OpenAI...');
+
 const response = await generateText(
-'Write one short German ecommerce sentence.'
+  'Write one short professional German ecommerce sentence.'
 );
 
+console.log('\nAI RESPONSE:\n');
 console.log(response);
+
+} catch (error) {
+console.error('\nERROR:\n');
+console.error(error);
+}
 }
 
 test();
