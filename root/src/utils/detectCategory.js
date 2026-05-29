591dc170b5d337b92c9637a02cb95730e7433676
sofracadrian-code <sofracadrian@yahoo.com>
2026-05-30 01:24:09 +0200

    Delete root/src directory

--- root/src/utils/detectCategory.js
+++
@@ -1,9 +0,0 @@
-export function detectCategory(product) {
-const title = product.title.toLowerCase();
-
-if (title.includes('smartphone')) return 'smartphone';
-if (title.includes('tablet')) return 'tablet';
-if (title.includes('roller')) return 'escooter';
-
-return 'general';
-}
